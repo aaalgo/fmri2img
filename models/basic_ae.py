1622b5f15fc16af81037b7564464c8e2e670c3ac
Wei Dong <wdong@aaalgo.com>
2023-03-27 17:25:30 -0400

    update

--- models/basic_ae.py
+++ models/basic_ae.py
@@ -6,20 +6,23 @@ from config import *
 class FmriEncoder (nn.Module):
     def __init__ (self, Din):
         super().__init__()
-        self.fc1 = nn.Linear(Din, 512)
-        self.fc2 = nn.Linear(512, 256)
-        self.deconv1 = nn.ConvTranspose2d(16, 16, kernel_size=4, stride=2, padding=1)
-        self.deconv2 = nn.ConvTranspose2d(16, 4, kernel_size=4, stride=2, padding=1)
+        self.fc1 = nn.Linear(Din, 2048)
+        self.fc2 = nn.Linear(2048, 2048)
+        self.fc3 = nn.Linear(2048, 1024)
+        self.deconv1 = nn.ConvTranspose2d(16, 4, kernel_size=4, stride=2, padding=1)
+        #self.deconv2 = nn.ConvTranspose2d(16, 4, kernel_size=4, stride=2, padding=1)
 
     def forward (self, X):
         x = self.fc1(X)
         x = nn.functional.gelu(x)
         x = self.fc2(x)
         x = nn.functional.gelu(x)
-        x = x.view(-1, 16, 4, 4)
-        x = self.deconv1(x)
+        x = self.fc3(x)
         x = nn.functional.gelu(x)
-        x = self.deconv2(x)
+        x = x.view(-1, 16, 8, 8)
+        x = self.deconv1(x)
+        #x = nn.functional.gelu(x)
+        #x = self.deconv2(x)
         return x
 
 class Fmri2Image (nn.Module):
